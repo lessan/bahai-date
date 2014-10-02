bc69d9f1be235aac5aaacf126cdf7e861c07e2a0
Lessan Vaezi <lessan@gmail.com>
2014-10-02 17:03:32 -0400

    Conversion from gregorian dates

--- lib/bahai_date/bahai_date.rb
+++ lib/bahai_date/bahai_date.rb
@@ -1,21 +1,35 @@
 require 'date'
+require_relative 'year'
+require_relative 'month'
+require_relative 'day'
+require_relative 'weekday'
 
 module BahaiDate
 
   class BahaiDate
     attr_reader :weekday, :day, :month, :year, :gregorian_date
 
-    def self.new_from_YMD(year, month, day)
-      gregorian_date = to_gregorian(year, month, day)
-      self.new(year, month, day, gregorian_date)
+    def initialize(params)
+      if params[:date]
+        parse_Date params[:date]
+      elsif params[:year] and params[:month] and params[:day]
+        parse_YMD params[:year], params[:month], params[:day]
+      else
+        raise ArgumentError, "Invalid arguments. Use a hash with :date or with :year, :month, and :day."
+      end
     end
 
-    def self.new_from_Date(date)
-      year, month, day = from_gregorian(@gregorian_date)
-      self.new(year, month, day, date)
+    def parse_YMD(year, month, day)
+      gregorian_date = self.class.to_gregorian(year, month, day)
+      set_attributes(year, month, day, gregorian_date)
     end
 
-    def initialize(year, month, day, gregorian_date)
+    def parse_Date(date)
+      year, month, day = self.class.from_gregorian(date)
+      set_attributes(year, month, day, date)
+    end
+
+    def set_attributes(year, month, day, gregorian_date)
       @gregorian_date = gregorian_date
       @year = Year.new(year)
       @month = Month.new(month)
@@ -24,11 +38,36 @@ module BahaiDate
     end
 
     def self.to_gregorian(year, month, day)
-      Date.new(1844,5,23)
+      Date.new(1844,3,21)
     end
 
     def self.from_gregorian(date)
-      [1, 1, 1]
+      nawruz = YearData.nawruz_for(date.year)
+
+      year = date.year - 1844
+      if date >= nawruz then
+        year += 1
+        days = (date - nawruz).to_i
+      else
+        days = (date - YearData.nawruz_for(date.year - 1)).to_i
+      end
+ 
+      # determine day and month, taking into account ayyam-i-ha
+      if days >= 342
+        ayyam_i_ha = YearData.is_leap?(year) ? 5 : 4
+        if days < (342 + ayyam_i_ha)
+          month = -1
+          day = days - 342
+        else
+          month = 19
+          day = days - (342 + ayyam_i_ha)
+        end
+      else
+        month, day = (days).divmod(19)
+        month += 1
+      end
+      day += 1
+      [year, month, day]
     end
 
     def self.weekday_from_gregorian(date)
@@ -38,4 +77,16 @@ module BahaiDate
 
   end
 
+
+  class YearData
+    LEAP_YEARS = [4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108, 112, 116, 120, 124, 128, 132, 136, 140, 144, 148, 152, 156, 160, 164, 168]
+
+    def self.nawruz_for(year)
+      Date.new(year, 3, 21)
+    end
+
+    def self.is_leap?(year)
+      LEAP_YEARS.include? year
+    end
+  end
 end
--- spec/bahai_date/bahai_date_spec.rb
+++ spec/bahai_date/bahai_date_spec.rb
@@ -4,26 +4,27 @@ module BahaiDate
 
   describe BahaiDate do
 
-    it "can be created using the initializer" do
-      bahai_date = BahaiDate.new(1, 1, 1, Date.today)
-
-      expect(bahai_date).to_not be_nil
-    end
-
     it "can be created from a Year, Month and Day" do
-      bahai_date = BahaiDate.new_from_YMD(1, 1, 1)
+      bahai_date = BahaiDate.new(year: 1, month: 1, day: 1)
 
       expect(bahai_date).to_not be_nil
     end
 
     it "can be created from a gregorian Date object" do
-      bahai_date = BahaiDate.new_from_Date(Date.new(2010, 1, 1))
+      bahai_date = BahaiDate.new(date: Date.new(2010, 1, 1))
 
       expect(bahai_date).to_not be_nil
     end
 
+    it "raises an error if invalid arguments are passed to the initializer" do
+      expect { BahaiDate.new }.to raise_error(ArgumentError)
+      expect { BahaiDate.new({}) }.to raise_error(ArgumentError)
+      expect { BahaiDate.new(day: 1) }.to raise_error(ArgumentError)
+    end
+
+
     it "exposes various attributes" do
-      bahai_date = BahaiDate.new_from_YMD(1, 1, 1)
+      bahai_date = BahaiDate.new(year: 1, month: 1, day: 1)
 
       expect(bahai_date.weekday).to be_an_instance_of(Weekday) 
       expect(bahai_date.day).to be_an_instance_of(Day) 
@@ -32,46 +33,89 @@ module BahaiDate
       expect(bahai_date.gregorian_date).to be_an_instance_of(Date)
     end
 
-    it "can convert from a gregorian date" do
+    it "can convert to a gregorian date" do
       gregorian_date = BahaiDate.to_gregorian(1, 1, 1)
 
       expect(gregorian_date).to be_an_instance_of(Date)
-      expect(gregorian_date).to eq(Date.new(1844,5,23))
+      expect(gregorian_date).to eq(Date.new(1844,3,21))
     end
 
-    it "can convert to a gregorian date" do
-      year, month, day = BahaiDate.from_gregorian(Date.new(1844,5,23))
-
+    it "can convert from a gregorian date" do
+      year, month, day = BahaiDate.from_gregorian(Date.new(1844,3,21))
       expect(year).to eq(1)
       expect(month).to eq(1)
       expect(day).to eq(1)
+
+      year, month, day = BahaiDate.from_gregorian(Date.new(1845,3,20))
+      expect(year).to eq(1)
+      expect(month).to eq(19)
+      expect(day).to eq(19)
+
+      year, month, day = BahaiDate.from_gregorian(Date.new(1845,3,21))
+      expect(year).to eq(2)
+      expect(month).to eq(1)
+      expect(day).to eq(1)
+
+      year, month, day = BahaiDate.from_gregorian(Date.new(2012,2,25))
+      expect(year).to eq(168)
+      expect(month).to eq(18)
+      expect(day).to eq(19)
+ 
+      year, month, day = BahaiDate.from_gregorian(Date.new(2012,2,26))
+      expect(year).to eq(168)
+      expect(month).to eq(-1)
+      expect(day).to eq(1)
+
+      year, month, day = BahaiDate.from_gregorian(Date.new(2012,3,1))
+      expect(year).to eq(168)
+      expect(month).to eq(-1)
+      expect(day).to eq(5)
+
+      year, month, day = BahaiDate.from_gregorian(Date.new(2012,3,2))
+      expect(year).to eq(168)
+      expect(month).to eq(19)
+      expect(day).to eq(1)
+
+      year, month, day = BahaiDate.from_gregorian(Date.new(2013,3,1))
+      expect(year).to eq(169)
+      expect(month).to eq(-1)
+      expect(day).to eq(4)
     end
 
     it "can get weekday from a gregorian date accurately" do
-      weekday = BahaiDate.weekday_from_gregorian(Date.new(1844,5,23))
+      weekday = BahaiDate.weekday_from_gregorian(Date.new(1844,3,21))
       expect(weekday).to eq(6)
 
-      weekday = BahaiDate.weekday_from_gregorian(Date.new(1844,5,24))
+      weekday = BahaiDate.weekday_from_gregorian(Date.new(1844,3,22))
       expect(weekday).to eq(7)
 
-      weekday = BahaiDate.weekday_from_gregorian(Date.new(1844,5,25))
+      weekday = BahaiDate.weekday_from_gregorian(Date.new(1844,3,23))
       expect(weekday).to eq(1)
 
-      weekday = BahaiDate.weekday_from_gregorian(Date.new(1844,5,26))
+      weekday = BahaiDate.weekday_from_gregorian(Date.new(1844,3,24))
       expect(weekday).to eq(2)
 
-      weekday = BahaiDate.weekday_from_gregorian(Date.new(1844,5,27))
+      weekday = BahaiDate.weekday_from_gregorian(Date.new(1844,3,25))
       expect(weekday).to eq(3)
 
-      weekday = BahaiDate.weekday_from_gregorian(Date.new(1844,5,28))
+      weekday = BahaiDate.weekday_from_gregorian(Date.new(1844,3,26))
       expect(weekday).to eq(4)
 
-      weekday = BahaiDate.weekday_from_gregorian(Date.new(1844,5,29))
+      weekday = BahaiDate.weekday_from_gregorian(Date.new(1844,3,27))
       expect(weekday).to eq(5)
 
-      weekday = BahaiDate.weekday_from_gregorian(Date.new(1844,5,30))
+      weekday = BahaiDate.weekday_from_gregorian(Date.new(1844,3,28))
       expect(weekday).to eq(6)
    end
   end
 
+  describe YearData do
+
+    it "determines whether a year is leap or not" do
+      expect(YearData.is_leap?(1)).to eq(false)
+      expect(YearData.is_leap?(4)).to eq(true)
+      expect(YearData.is_leap?(168)).to eq(true)
+    end
+
+  end
 end
